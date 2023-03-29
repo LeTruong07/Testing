156fd78eca070e7b4ea53920d76f7525e4ae73a5
Le Truong <letruong07042004@gmail.com>
2023-03-29 16:31:54 +0700

    deleted MaxIndex

--- MaxIndex.py
+++
@@ -1,7 +0,0 @@
-mylist = []
-for i in range(8):
-  mountain_h = int(input())
-  mylist.append(mountain_h)
-while not max(mylist) == 0 :
- print(mylist.index(max(mylist)))
- mylist[mylist.index(max(mylist))] = 0
